e1eb8b0967f0b22ae5f0339e6b3bc2dd3e0e2b5d
RikhilTaneja <rikhiltaneja@gmail.com>
2024-03-19 11:03:49 +0530

    schema initiated

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
