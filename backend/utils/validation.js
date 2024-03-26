b88e28a7bef3d27c8512ecd88294ac00f9535ea7
rikhil7 <rikhiltaneja007@gmail.com>
2024-03-26 12:39:32 +0530

    password hashing and login functionality developed

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,10 +1,14 @@
 const Joi = require("joi");
 
 exports.userValidation = Joi.object({
-    name: Joi.string().required(),
-    username: Joi.string().required(),
-    institution: Joi.string().required(),
-    password: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.number().required(),
-})
+  name: Joi.string().required(),
+  username: Joi.string().required(),
+  institution: Joi.string().required(),
+  password: Joi.string().required(),
+  contact: Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    phone: Joi.number().integer().min(1111111111).max(9999999999).required(),
+  }).required(),
+});
