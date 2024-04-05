84357644490d7dae38df9cc6edad2dc406c8183d
Rikhil <rikhiltaneja@gmail.com>
2024-04-05 13:33:01 +0530

    institution schema and joi validation developed

---
+++ backend/models/institution.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const instituionsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    phone: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+});
+
+const Institution = mongoose.model("Institution", instituionsSchema);
+
+module.exports = Institution;
